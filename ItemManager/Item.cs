c7303dcb3abcb4b223ae6c91238a4e701755de27
Wise389 <50751786+Wise389@users.noreply.github.com>
2019-08-21 13:45:50 -0400

    Add files via upload

---
+++ ItemManager/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ItemManager.Models
+{
+    string name;
+    int weight;
+    bool equip;
+    SqlConnection con= new SqlConnection("Data Source=.;Initial Catalog=Sample;Integrated Security=true;");  
+    SqlCommand cmd;  
+    SqlDataAdapter adapt;  
+    //ID variable used in Updating and Deleting Record  
+    int ID = 0; 
+
+    if (txt_Name.Text != "" && txt_State.Text != "") {  
+    cmd = new SqlCommand("insert into tbl_Record(Name,State) values(@name,@state)", con);  
+    con.Open();  
+    cmd.Parameters.AddWithValue("@name", txt_Name.Text);  
+    cmd.Parameters.AddWithValue("@state", txt_State.Text);  
+    cmd.ExecuteNonQuery();  
+    con.Close();  
+    MessageBox.Show("Record Inserted Successfully");  
+    DisplayData();  
+    ClearData();  
+    } else {  
+    MessageBox.Show("Please Provide Details!");  
+    }  
+
+    class Item
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+//Id: int
+//Name: string
+//Weight: int
+//Rating: float
+//Wearable: Bool
---
+++ ItemManager/ItemController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemManager.Controllers
+{
+    class ItemController
+    {
+        static void Main(string[] args)
+        {
+            
+//Run(): void
+class run
+{
+    public void run(string answer)
+    {
+
+    }
+}
+
+//Private CreateItem(): void
+class CreateItem
+{
+    Item NewItem = new Item();
+}
+//Private DisplayItems(): void
+class DisplayItems
+{
+    private void DisplayItems(name)
+    {
+        for (i = 0; i <= Array.length; i++)
+        {
+            Console.WriteLine(Item[i]);
+        }
+        Console.ReadLine();
+    }
+}
+//Private SearchItems(): void
+class SearchItems
+{
+    private void SearchItems(identifier)
+    {
+        //lookup
+    }
+}
+//Private EditItem(): void
+class EditItem
+{
+    private void EditItem(int i, string newName)
+    {
+        Item[i] = newName;
+    }
+}
+//Private RemoveItem(): void
+class RemoveItem
+{
+    private void RemoveItem(int i, string Item)
+    {
+        delete.item[i];
+    }
+}
+
+
+
+
+
+
